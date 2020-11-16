5cb76ab7fd87946930c73b6591dc603b3fc5605f
Kaviilee <jiaweilee@126.com>
2020-11-17 00:17:41 +0800

    fix: 修复 sql 没有 token 字段的问题

--- sql/todolist.sql
+++ sql/todolist.sql
@@ -1,59 +1,56 @@
-/*
- Navicat Premium Data Transfer
+-- --------------------------------------------------------
+-- 主机:                           127.0.0.1
+-- 服务器版本:                        5.7.32 - MySQL Community Server (GPL)
+-- 服务器操作系统:                      Linux
+-- HeidiSQL 版本:                  11.1.0.6116
+-- --------------------------------------------------------
 
- Source Server         : 3306
- Source Server Type    : MySQL
- Source Server Version : 50714
- Source Host           : localhost:3306
- Source Schema         : todolist
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 50714
- File Encoding         : 65001
 
- Date: 13/07/2020 15:45:42
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
 -- 导出 todolist 的数据库结构
-CREATE DATABASE IF NOT EXISTS `todolist` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `todolist` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `todolist`;
 
--- ----------------------------
--- Table structure for list
--- ----------------------------
-DROP TABLE IF EXISTS `list`;
-CREATE TABLE `list`  (
+-- 导出  表 todolist.list 结构
+CREATE TABLE IF NOT EXISTS `list` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'list的id',
-  `userId` int(11) NULL DEFAULT NULL COMMENT '用户id',
-  `content` char(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'list内容',
-  `status` tinyint(1) NULL DEFAULT NULL COMMENT 'list的状态',
+  `userId` int(11) DEFAULT NULL COMMENT '用户id',
+  `content` char(255) DEFAULT NULL COMMENT 'list内容',
+  `status` tinyint(1) DEFAULT NULL COMMENT 'list的状态',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
 
--- ----------------------------
--- Records of list
--- ----------------------------
-INSERT INTO `list` VALUES (8, 1, 'test212', 0);
-INSERT INTO `list` VALUES (6, 1, 'test', 0);
-INSERT INTO `list` VALUES (7, 1, 'test213', 0);
-INSERT INTO `list` VALUES (9, 1, 'test21221', 0);
+-- 正在导出表  todolist.list 的数据：4 rows
+/*!40000 ALTER TABLE `list` DISABLE KEYS */;
+INSERT INTO `list` (`id`, `userId`, `content`, `status`) VALUES
+	(8, 1, 'test212', 0),
+	(6, 1, 'test', 1),
+	(7, 1, 'test213', 0),
+	(9, 1, 'test21221', 0);
+/*!40000 ALTER TABLE `list` ENABLE KEYS */;
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+-- 导出  表 todolist.user 结构
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `userName` char(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名字',
-  `password` char(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户密码',
+  `userName` char(50) DEFAULT NULL COMMENT '用户名字',
+  `password` char(128) DEFAULT NULL COMMENT '用户密码',
+  `token` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 'kavii', '123');
+-- 正在导出表  todolist.user 的数据：1 rows
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `userName`, `password`, `token`) VALUES
+	(1, 'kavii', '123', '');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
